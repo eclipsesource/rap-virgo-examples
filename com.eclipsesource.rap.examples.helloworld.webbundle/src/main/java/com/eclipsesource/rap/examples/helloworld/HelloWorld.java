76cc453f21d214ea40bbd313449dc752f5e8e856
Florian Waibel <fwaibel@eclipsesource.com>
2012-10-22 00:12:53 +0200

    Added WAB example com.eclipsesource.rap.examples.helloword.webbundle

---
+++ com.eclipsesource.rap.examples.helloworld.webbundle/src/main/java/com/eclipsesource/rap/examples/helloworld/HelloWorld.java
@@ -0,0 +1,55 @@
+package com.eclipsesource.rap.examples.helloworld;
+
+import org.eclipse.rap.rwt.lifecycle.IEntryPoint;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
+public class HelloWorld implements IEntryPoint {
+
+	private String buttonText = "+1";
+
+	public void setButtonText(String buttonText) {
+		this.buttonText = buttonText;
+	}
+
+	public int createUI() {
+		Display display = new Display();
+		Shell mainShell = new Shell(display, SWT.TITLE);
+		mainShell.setText("RAP Hello World!");
+		mainShell.setMaximized(true);
+		mainShell.setLayout(new FillLayout());
+
+		Composite parent = new Composite(mainShell, SWT.BORDER);
+		parent.setLayout(new GridLayout());
+
+		Button button = new Button(parent, SWT.PUSH);
+		button.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false));
+		button.setText(buttonText);
+
+		final Label label = new Label(parent, SWT.NONE);
+		label.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false));
+		label.setText("No event yet.");
+		button.addSelectionListener(new SelectionAdapter() {
+			int counter = 0;
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				super.widgetSelected(e);
+				label.setText("Clicked" + " [" + ++counter + "]");
+			}
+		});
+
+		mainShell.layout();
+		mainShell.open();
+		return 0;
+	}
+}
---
+++ com.eclipsesource.rap.examples.helloworld.webbundle/src/main/java/com/eclipsesource/rap/examples/helloworld/HelloWorldConfiguration.java
@@ -0,0 +1,19 @@
+package com.eclipsesource.rap.examples.helloworld;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.rap.rwt.application.Application;
+import org.eclipse.rap.rwt.application.ApplicationConfiguration;
+import org.eclipse.rap.rwt.client.WebClient;
+
+public class HelloWorldConfiguration implements ApplicationConfiguration {
+
+	@Override
+	public void configure(Application application) {
+		Map<String, String> properties = new HashMap<String, String>();
+		properties.put(WebClient.PAGE_TITLE, "Hello world");
+		application.addEntryPoint("/hello", HelloWorld.class, properties);
+	}
+
+}
